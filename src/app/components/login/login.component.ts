35d3ab8eb3e9b064b5678eb4e9db5c0b89d4506f
Rithik Suthan S <rithiksuthan.smartail@gmail.com>
2024-05-03 16:34:25 +0530

    added google authentication

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
+  hide:any;
   ngOnInit(): void {
+  this.hide=true;
   }
-
+  showPassword()
+  {
+    this.hide=!this.hide;
+  }
+  signUp()
+  {
+    this.router.navigateByUrl("/register"); 
+  }
 }
