7aba517427cd2da73a93ec4b9214e0891e6dd7f2
Rithik Suthan S <rithiksuthan.smartail@gmail.com>
2024-05-07 10:18:08 +0530

    added absolute for footer

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   version: '0.0.1',         
-  employee_service_url:"https://employee-management-422016.el.r.appspot.com",
+  // employee_service_url:"https://employee-management-422016.el.r.appspot.com",
+  employee_service_url:"http://localhost:8080",
 };
