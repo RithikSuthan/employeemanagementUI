1cabf8b2b5ae8b1f1a933d9547ee95e7b9234f5d
Rithik Suthan S <rithiksuthan.smartail@gmail.com>
2024-05-13 14:42:04 +0530

    added android capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'staff-sync',
+  webDir: 'dist/employeemanagementUI'
+};
+
+export default config;
