5f97a72e983d27147e11718b10bc31d67b503f8d
Rithik Suthan S <rithiksuthan.smartail@gmail.com>
2024-06-06 13:41:56 +0530

    added time constrain for deadline

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -13,6 +13,7 @@ export class TasksComponent implements OnInit {
   loadManagers:any;
   loadManagerEmployees:any;
   backupManagerTask:any;
+  minDateTime: any;
   employee_data: any = {
     tasks: []
   };
@@ -31,6 +32,7 @@ export class TasksComponent implements OnInit {
   ngOnInit(): void {
     this.loadDashboard();
     this.fetchManagers();
+    this.setMinDateTime();
   }
   dropDown(event:any)
   {
@@ -201,5 +203,14 @@ fetchManagerEmployee()
               console.log(error);
             }
       )
+  }
+  setMinDateTime(): void {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = ('0' + (now.getMonth() + 1)).slice(-2);
+    const day = ('0' + now.getDate()).slice(-2);
+    const hours = ('0' + now.getHours()).slice(-2);
+    const minutes = ('0' + now.getMinutes()).slice(-2);
+    this.minDateTime = `${year}-${month}-${day}T${hours}:${minutes}`;
   }
 }
