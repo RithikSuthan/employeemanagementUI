552518512de5f19767e3fba3abbe7ef2021616c6
Rithik Suthan S <rithiksuthan.smartail@gmail.com>
2024-12-24 22:13:05 +0530

    updated mailbox linl from gcloud to render

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: true,
   version: '2.0.1',         
   employee_service_url:"https://employee-management-422016.el.r.appspot.com",
-  email_service_url:"https://mailboxexpress.el.r.appspot.com"
+  // email_service_url:"https://mailboxexpress.el.r.appspot.com"
+  email_service_url:"https://mailbox-express-k9hn.onrender.com"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,6 @@ export const environment = {
   employee_service_url:"https://employee-management-422016.el.r.appspot.com",
   // employee_service_url:"http://localhost:8080",
   // email_service_url:"http://127.0.0.1:5000"
-  email_service_url:"https://mailboxexpress.el.r.appspot.com"
+  // email_service_url:"https://mailboxexpress.el.r.appspot.com"
+  email_service_url:"https://mailbox-express-k9hn.onrender.com"
 };
