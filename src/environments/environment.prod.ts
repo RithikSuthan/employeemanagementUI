295b5dcc7597d71419d292314287ab4aad62c1c2
Rithik Suthan S <rithiksuthan.smartail@gmail.com>
2024-12-25 12:38:13 +0530

    updated gcloud link to render link version 2.0.2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
-  version: '2.0.1',         
-  employee_service_url:"https://employee-management-422016.el.r.appspot.com",
+  version: '2.0.2',         
+  // employee_service_url:"https://employee-management-422016.el.r.appspot.com",
+  employee_service_url:"https://staffsyncservice.onrender.com",
   // email_service_url:"https://mailboxexpress.el.r.appspot.com"
   email_service_url:"https://mailbox-express-k9hn.onrender.com"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: false,
-  version: '2.0.1',         
-  employee_service_url:"https://employee-management-422016.el.r.appspot.com",
+  version: '2.0.2',         
+  // employee_service_url:"https://employee-management-422016.el.r.appspot.com",
+  employee_service_url:"https://staffsyncservice.onrender.com",
   // employee_service_url:"http://localhost:8080",
   // email_service_url:"http://127.0.0.1:5000"
   // email_service_url:"https://mailboxexpress.el.r.appspot.com"
